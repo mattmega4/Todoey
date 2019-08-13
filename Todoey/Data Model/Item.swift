6340ae58b89bb13f2ac389b2266cd43b1a8f77d2
mattmega4 <mattmega4@gmail.com>
2019-08-12 23:09:18 -0700

    Convert to MCV design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Matthew Howes Singleton on 8/12/19.
+//  Copyright © 2019 Matthew Howes Singleton. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title: String = ""
+  var done: Bool = false
+}
